05c4b0ed295c426751d25a6bb867f52ee070219f
vepe99 <114901599+vepe99@users.noreply.github.com>
2025-07-09 08:36:13 +0000

    Deploying to gh-pages from @ vepe99/vepe99.github.io@5bdf9f6d5a9c1957ec471c532e6be26905027f6a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,13 +462,6 @@ ninja.data = [{
           window.open("https://github.com/vepe99", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
         id: 'social-orcid',
         title: 'ORCID',
         section: 'Socials',
@@ -487,14 +480,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=SDjHaYAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
